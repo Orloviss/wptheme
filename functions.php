f6f54d4bda797058172abdd83c20d5f6fdf1a60e
Orloviss <44546222+Orloviss@users.noreply.github.com>
2025-06-06 11:31:13 +0300

    Update functions.php
    Fix svg loading

--- functions.php
+++ functions.php
@@ -153,43 +153,49 @@ function wptheme_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'wptheme_scripts' );
 
+
 /**
  * Svg support
  */
 
-function allow_svg_uploads( $mime_types ) {
-    $mime_types['svg'] = 'image/svg+xml';
-    return $mime_types;
+add_filter( 'upload_mimes', 'svg_upload_allow' );
+
+# Добавляет SVG в список разрешенных для загрузки файлов.
+function svg_upload_allow( $mimes ) {
+	$mimes['svg']  = 'image/svg+xml';
+
+	return $mimes;
 }
-add_filter( 'upload_mimes', 'allow_svg_uploads' );
+add_filter( 'wp_check_filetype_and_ext', 'fix_svg_mime_type', 10, 5 );
 
+# Исправление MIME типа для SVG файлов.
+function fix_svg_mime_type( $data, $file, $filename, $mimes, $real_mime = '' ){
 
-function sanitize_svg_file( $file ) {
-    if ( 'image/svg+xml' === $file['type'] ) {
-        // Get SVG content
-        $svg_content = file_get_contents( $file['tmp_name'] );
+	// WP 5.1 +
+	if( version_compare( $GLOBALS['wp_version'], '5.1.0', '>=' ) ){
+		$dosvg = in_array( $real_mime, [ 'image/svg', 'image/svg+xml' ] );
+	}
+	else {
+		$dosvg = ( '.svg' === strtolower( substr( $filename, -4 ) ) );
+	}
 
-        // Use SVG Sanitizer
-        require_once ABSPATH . 'vendor/autoload.php';
-        $sanitizer = new \enshrined\svgSanitize\Sanitizer();
-        $clean_svg = $sanitizer->sanitize( $svg_content );
+	// mime тип был обнулен, поправим его
+	// а также проверим право пользователя
+	if( $dosvg ){
 
-        if ( $clean_svg ) {
-            // Write sanitized content back to file
-            file_put_contents( $file['tmp_name'], $clean_svg );
-        } else {
-            // If sanitization fails, return an error
-            $file['error'] = __( 'Sorry, the SVG file could not be sanitized.' );
-        }
-    }
-    return $file;
+		// разрешим
+		if( current_user_can('manage_options') ){
+
+			$data['ext']  = 'svg';
+			$data['type'] = 'image/svg+xml';
+		}
+		// запретим
+		else {
+			$data['ext']  = false;
+			$data['type'] = false;
+		}
+
+	}
+
+	return $data;
 }
-add_filter( 'wp_handle_upload_prefilter', 'sanitize_svg_file' );
-
-function restrict_svg_uploads_to_admins( $mime_types ) {
-    if ( ! current_user_can( 'administrator' ) ) {
-        unset( $mime_types['svg'] );
-    }
-    return $mime_types;
-}
-add_filter( 'upload_mimes', 'restrict_svg_uploads_to_admins' );
